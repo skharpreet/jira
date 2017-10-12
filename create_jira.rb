e4bcec44f7ce96b3ba7f76c328343fa2b75b1ac9
Harpreet Singh <sk.harpreet@gmail.com>
2017-10-12 08:12:56 +0530

    Improvisation and more parameters added

--- create_jira.rb
+++ create_jira.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'jira-ruby'
 require 'io/console'
-args = Hash[ ARGV.flat_map{|s| s.scan(/--?([^=\s]+)(?:=([a-zA-Z0-9_ ]*))?/) } ]
+args = Hash[ ARGV.flat_map{|s| s.scan(/--?([^=\s]+)(?:=([a-zA-Z0-9_ @\.]*))?/) } ]
 
-if(args.empty?)
-  p "-cloud=https://xxx.atlassian.net -project=abc -title=title -desc=description -type=bug/task -assignee=Harpreet Singh -email=harpreet_singh@gmail.com -p"
+if(args.empty? || (args.has_key?('help') && args['help'].nil?))
+  puts "ruby create_jira.rb -title=title -desc=description -type=bug/task -email=harpreet_singh@gmail.com -p"
+  puts "Optional params: -cloud=https://xxx.atlassian.net -project=abc"
+  abort
 end
 
 if(args["p"].nil? || args["P"].nil?)
@@ -12,11 +14,11 @@ if(args["p"].nil? || args["P"].nil?)
   password = STDIN.noecho(&:gets).chop
 end
 
-jira_cloud = args['cloud']
-project_name = args['project']
+jira_cloud = args['cloud'] || 'https://xxx.atlassian.net'
+project_name = args['project'] || 'xxx'
 
 puts "\nStarting ..."
-
+p args
 options = {
   :username     => "#{args['email']}",
   :password     => "#{password}",
@@ -37,10 +39,10 @@ begin
   issue_type = issue_types[args['type']]
 
   issue = client.Issue.build
-  issue.save({"fields"=>{"summary"=>"#{args['title']}","project"=>{"id"=>"#{project.id}"},"issuetype"=>{"id"=>"#{issue_type}"}, "description"=> "#{args['desc']}", "assignee" => {"name": "#{args['assignee']}"}}})
+  issue.save({"fields"=>{"summary"=>"#{args['title']}","project"=>{"id"=>"#{project.id}"},"issuetype"=>{"id"=>"#{issue_type}"}, "description"=> "#{args['desc']}"}})
   issue.fetch
 
-  puts "\nOpening now..."
+  puts "\nTicket number #{issue.key} is created. Opening now..."
   system("open", "#{jira_cloud}/browse/#{issue.key}")
 rescue Exception => e
   puts "\nProblem in the api options. #{e.message}"
